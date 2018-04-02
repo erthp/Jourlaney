6c902a82efda266be61a885ffafabb9973524dee
Erth <panot1997@hotmail.com>
2018-04-02 13:32:00 +0700

    Add header file

---
+++ resources/views/header.blade.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Jourlaney</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        crossorigin="anonymous">
+    <link rel="stylesheet" href="css/jourlaney.css">
+    <link href="{{ URL::asset('css/jourlaney.css') }}" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <div class="container">
+        <div class="homepic">
+            <nav class="navbar navbar-light clearbg">
+                <a class="navbar-brand clearbg titletext" href="#">
+                    <img src="favicon.png" width="30" height="30" class="d-inline-block align-top=" alt="">
+                    <span class="titletext">Jourlaney</span>
+                </a>
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <div class="btn-nav">
+                            <a class="btn btn-outline-light btn-small navbar-btn clearbg" href="">Sign up</a>
+                            <a class="btn btn-outline-light btn-small navbar-btn clearbg" href="">Log in</a>
+                        </div>
+                    </li>
+                </ul>
+            </nav>
+            <div>
+                <h1 class="hometext">Let's tour
+                    <br/>THAILAND</h1>
+            </div>
