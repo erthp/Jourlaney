7961c3ce32681d78d176ea11f2f9bb8b247e4eeb
Erth <panot1997@hotmail.com>
2018-04-02 11:30:22 +0700

    Test routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('signin',function(){
+    return view('signin');
+});
+
+Route::get('register',function(){
+    return view('register');
+});
