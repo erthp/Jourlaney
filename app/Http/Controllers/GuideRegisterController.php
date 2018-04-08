76106c287553a846ca050ea81c7efc068428945c
Erth <panot1997@hotmail.com>
2018-04-09 00:28:06 +0700

    Add query

--- app/Http/Controllers/GuideRegisterController.php
+++ app/Http/Controllers/GuideRegisterController.php
@@ -11,8 +11,9 @@ use App\Models\GuideRegister;
 class GuideRegisterController extends Controller
 {
     public function guideregis(Request $request){
-        $queryUser = DB::insert("insert into Users(username,userPassword,userDOB) values(?,?,?)",[$request->input('username'),$request->input('password'),$request->input('birthdate')]);
-        $queryGuide = DB::insert("insert into Guide(guideLicenseNumber) value(?)",[$request->input('guidelicense')]);
-        echo $queryUser, $queryGuide;
+        $username = $request->input('username');
+        $queryUser = DB::insert("insert into Users(username,userPassword,userFirstName,userLastName,userEmail,userGender,userDOB,userIdcard) values(?,?,?,?,?,?,?,?)",[$username,$request->input('password'),$request->input('firstname'),$request->input('lastname'),$request->input('email'),$request->input('gender'),$request->input('birthdate'),$request->input('idcard')]);
+        $queryguide = DB::insert("insert into Guide(username,guideLicenseNumber) value(?,?)",$username,[$request->input('guidelicense')]);
+        echo $queryUser,$queryguide;
     }
 }
