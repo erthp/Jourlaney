6da0e630d2eb43af96a153afb47ab178581dbd27
Erth <panot1997@hotmail.com>
2018-04-08 13:44:59 +0700

    Add function

--- app/Http/Controllers/GuideRegisterController.php
+++ app/Http/Controllers/GuideRegisterController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\DB;
 
 class GuideRegisterController extends Controller
 {
-    //
+    public function guideregis(Request $request){
+        $result = DB::insert("insert into ");
+    }
 }
