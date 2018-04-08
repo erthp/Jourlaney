192cae925d1ad4b32777f87e3612761eefb3634a
faiiptw <faii_ptw@hotmail.com>
2018-04-08 15:28:12 +0700

    Add tourist register page

---
+++ resources/views/touristregister.blade.php
@@ -0,0 +1,43 @@
+@extends('header')
+@section('page')
+<div class="container">
+<div class="col-12 register-bg">
+                <h3 class="text-center register-header">Sign up to tourist</h3>
+
+                <form method="POST" name="register-form" action="{{URL::to('/guideregis')}}">
+                <div class="form-group">
+                    <label class="register-label" for="username">Username</label>
+                    <input type="text" class="form-control" id="username" placeholder="Username">
+                </div>
+                <div class="form-group">
+                    <label class="register-label" for="password">Password</label>
+                    <input type="password" class="form-control" id="password" placeholder="Password">
+                </div>
+                <div class="form-group">
+                    <label class="register-label" for="password">Re-password</label>
+                    <input type="repassword" class="form-control" id="repassword" placeholder="Re-password">
+                </div>
+                <div class="form-group">
+                    <label class="register-label" for="password">Email</label>
+                    <input type="email" class="form-control" id="email" placeholder="Email">
+                </div>
+                <div class="form-group">
+                    <label class="register-label" for="password">Birthdate</label>
+                    <input type="date" class="form-control" id="birthdate" placeholder="Birthday">
+                </div>
+                <div class="form-group">
+                    <label class="register-label" for="password">ID card number</label>
+                    <input type="number" class="form-control" id="idcard" placeholder="ID card number">
+                </div>
+                <div class="form-group">
+                    <label class="register-label" for="guidelicensepic">ID card picture</label>
+                    <a class='btn btn-primary' href='javascript:;'>
+                        Choose File...
+                        <input type="file" name="file_source" size="40"  onchange='$("#upload-file-info").html($(this).val());'>
+                    </a>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+</div>
+@endsection
