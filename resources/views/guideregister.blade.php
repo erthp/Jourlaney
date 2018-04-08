2687804b2183e9f320aa5b6b898a9c374ae64d0e
Erth <panot1997@hotmail.com>
2018-04-08 23:46:58 +0700

    Add first name, last name, gender field

--- resources/views/guideregister.blade.php
+++ resources/views/guideregister.blade.php
@@ -7,31 +7,52 @@
                 <form method="POST" name="register-form" action="{{URL::to('/guideregis')}}">
                 <div class="form-group">
                     <label class="register-label" for="username">Username</label>
-                    <input type="text" class="form-control" id="username" placeholder="Username">
+                    <input type="text" class="form-control" name="username" placeholder="Username">
                 </div>
                 <div class="form-group">
                     <label class="register-label" for="password">Password</label>
-                    <input type="password" class="form-control" id="password" placeholder="Password">
+                    <input type="password" class="form-control" name="password" placeholder="Password">
                 </div>
                 <div class="form-group">
                     <label class="register-label" for="repassword">Re-password</label>
-                    <input type="password" class="form-control" id="repassword" placeholder="Re-password">
+                    <input type="password" class="form-control" name="repassword" placeholder="Re-password">
+                </div>
+                <div class="form-group">
+                    <label class="register-label" for="firstname">First name</label>
+                    <input type="text" class="form-control" name="firstname" placeholder="First name">
+                </div>
+                <div class="form-group">
+                    <label class="register-label" for="lastname">Last name</label>
+                    <input type="text" class="form-control" name="lastname" placeholder="Last name">
                 </div>
                 <div class="form-group">
                     <label class="register-label" for="email">Email</label>
-                    <input type="email" class="form-control" id="email" placeholder="Email">
+                    <input type="email" class="form-control" name="email" placeholder="Email">
+                </div>
+                <label class="register-label" for="gender">Gender</label>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="gender" id="gender" value="Male">
+                    <label class="form-check-label" for="male">Male</label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="gender" id="gender" value="Female">
+                    <label class="form-check-label" for="female">Female</label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="gender" id="gender" value="Undefined">
+                    <label class="form-check-label" for="undefined">Undefined</label>
                 </div>
                 <div class="form-group">
                     <label class="register-label" for="birthdate">Birthdate</label>
-                    <input type="date" class="form-control" id="birthdate" placeholder="Birthday">
+                    <input type="date" class="form-control" name="birthdate" placeholder="Birthday">
                 </div>
                 <div class="form-group">
                     <label class="register-label" for="idcard">ID card number</label>
-                    <input type="number" class="form-control" id="idcard" placeholder="ID card number">
+                    <input type="number" class="form-control" name="idcard" placeholder="ID card number">
                 </div>
                 <div class="form-group">
                     <label class="register-label" for="guidelicense">Guide license number</label>
-                    <input type="number" class="form-control" id="guidelicense" placeholder="Guide license number">
+                    <input type="number" class="form-control" name="guidelicense" placeholder="Guide license number">
                     <input type="hidden" name="_token" value="{{csrf_token()}}">
                 </div>
                 <div class="form-group">
