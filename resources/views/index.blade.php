e445b87d6465642e6fa03cd85050078336b6c2f0
faiiptw <faii_ptw@hotmail.com>
2018-04-11 00:16:08 +0700

    add icon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,19 +4,19 @@
             <div class="row">
                 <div class="col-4">
                     <div align="center">
-                        <img src="favicon.png" class="homemenu-icon" height="80" alt="">
+                        <img src="../icon/medal.png" class="homemenu-icon" height="80" alt="">
                         <p class="homemenu-text">Recommend</p>
                     </div>
                 </div>
                 <div class="col-4">
                     <div align="center">
-                        <img src="favicon.png" class="homemenu-icon" height="80" alt="">
+                        <img src="../icon/bag.png" class="homemenu-icon" height="80" alt="">
                         <p class="homemenu-text">Create Trip</p>
                     </div>
                 </div>
                 <div class="col-4">
                     <div align="center">
-                        <img src="favicon.png" class="homemenu-icon" height="80" alt="">
+                        <img src="../icon/glasses.png" class="homemenu-icon" height="80" alt="">
                         <p class="homemenu-text">Find Guide</p>
                     </div>
                 </div>
