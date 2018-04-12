d4ddf08a9829a8cd4561eb3dca98a86a0aa11928
KnackKnack <kasidis.sr@mail.kmutt.ac.th>
2018-04-12 16:58:36 +0700

    Add ShowProfileGuide
    select table Guide

---
+++ app/Models/ShowProfileGuide.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ShowProfileUser extends Model
+    {
+       public $timestamps = false;
+       protected $table = 'Guide';
+    }
+    
