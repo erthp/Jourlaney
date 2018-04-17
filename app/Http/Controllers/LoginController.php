9fbb6886e3efab84b306777357b699cb754bd5dd
Erth <panot1997@hotmail.com>
2018-04-17 23:26:54 +0700

    Add checking function

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -9,5 +9,13 @@ class LoginController extends Controller
     public function login(request $req){
         $username = $req->input('username');
         $password = $req->input('password');
+
+        $check = DB::table('Users')->where(['username'=>$username,'password'=>$password])->get();
+        if(count($check) >0){
+            echo "login";
+        }
+        else{
+
+        }
     }
 }
