c27ec84942f32d4311ee3e11040883e10a7bfd7f
KnackKnack <kasidis.sr@mail.kmutt.ac.th>
2018-04-19 15:28:09 +0700

    Edit ShowProfileUser
    เชื่อม table showprofileuser

--- app/Models/ShowProfileUser.php
+++ app/Models/ShowProfileUser.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class ShowProfileUser extends Model
     {
        public $timestamps = false;
-       protected $table = 'User';
+       protected $table = 'Users'; //เลือกตาราง Users ให้แล้วนะจ๊ะ
     }
     
