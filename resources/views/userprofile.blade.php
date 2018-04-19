1987c946603f2b1bdea5e03ae0d33dd99751e317
KnackKnack <kasidis.sr@mail.kmutt.ac.th>
2018-04-19 15:28:52 +0700

    Add UserProfile(View)
    เขียนโครงแสดงข้อมูลของผู้ใช้ แต่ยังไม่ได้เชื่อมหน้า Login

---
+++ resources/views/userprofile.blade.php
@@ -0,0 +1,24 @@
+<html>  
+<head>
+<title>Jorlaney</title>
+</head>
+<body>
+  Welcome to User Profile<br>
+  <table border="1" style="width: 300px">
+    <tbody>
+      <tr>
+        <td width="87"> &nbsp;Username</td>
+        <td width="197"><?php echo $queryTourist["userFirstName"];?>
+        </td>
+      </tr>
+      <tr>
+        <td> &nbsp;Lastname</td>
+        <td><?php echo $queryTourist["userLastName"];?>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  <br>
+  
+</body>
+</html>
