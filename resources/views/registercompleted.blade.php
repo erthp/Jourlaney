85b2cdba2a253934cfeeed8160c201056229df7a
Erth <panot1997@hotmail.com>
2018-04-23 10:51:06 +0700

    Add log in form

--- resources/views/registercompleted.blade.php
+++ resources/views/registercompleted.blade.php
@@ -2,6 +2,22 @@
 @section('page')
 <div class="container">
     <h3 class="text-center register-header">Register Completed.</h3>
-        
+    <div class="row">
+        <div class="col-lg-2"></div>
+        <div class="col-lg-8">
+            <form method="POST" name="register-form" action="{{URL::to('/login')}">
+                <div class="form-group">
+                    <label class="login-label" for="username">Username</label>
+                    <input type="text" class="form-control" id="username" placeholder="Username">
+                </div>
+                <div class="form-group">
+                    <label class="login-label" for="password">Password</label>
+                    <input type="password" class="form-control" id="password" placeholder="Password">
+                </div>
+                <button type="submit" class="btn btn-success btn-block">Log in</button>
+            </form>
+        </div>
+        <div class="col-lg-2"></div>
+    </div>
 </div>
 @endsection
