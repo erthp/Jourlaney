2ebd63109e2d931403a728cb4e4deef00cb3cdc7
Erth <panot1997@hotmail.com>
2018-04-23 11:18:23 +0700

    Edit controller

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -7,8 +7,8 @@ use Illuminate\Http\Request;
 class LoginController extends Controller
 {
     public function login(request $req){
-        $username = $req->input('username');
-        $password = $req->input('password');
+        $username = $req->get('username');
+        $password = $req->get('password');
 
         $check = DB::table('Users')->where(['username'=>$username,'password'=>$password])->get();
         if(count($check) >0){
