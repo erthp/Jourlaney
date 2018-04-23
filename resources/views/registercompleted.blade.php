be28cff6e3e40fe890309aa35886fa47c3fe79e4
Erth <panot1997@hotmail.com>
2018-04-23 10:44:55 +0700

    Remove js

--- resources/views/registercompleted.blade.php
+++ resources/views/registercompleted.blade.php
@@ -3,13 +3,5 @@
 <div class="container">
     <h3 class="text-center register-header">Register Completed.</h3>
         
-
-    </div>
-<script>
-    $('#register-form').parsley();
-    $(document).ready(function(){
-        $('#idcard').mask('0-0000-00000-0');
-        $('.guidelicense').mask('00-00000');
-    });
-</script>
+</div>
 @endsection
