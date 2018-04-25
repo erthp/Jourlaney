1934325d00ff28aa6147676e1b71b13c48a336f5
Erth <panot1997@hotmail.com>
2018-04-25 17:50:09 +0700

    Add login function

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,19 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class LoginController extends Controller
 {
-    public function login(request $req){
+    public function login(Request $req){
         $username = $req->get('username');
         $password = $req->get('password');
 
-        $check = DB::table('Users')->where(['username'=>$username,'password'=>$password])->get();
-        if(count($check) >0){
-            echo "login";
+        $check = DB::table('Users')->where(['username'=>$username,'userPassword'=>$password])->get();
+        if(!is_null($check)){
+            session_start();
+            $req->session()->put('username', $username);
+            return redirect('/');
         }
         else{
-
+            echo "login failed.";
         }
     }
 }
