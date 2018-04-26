46861369a24a0cfa405e644815a06956fc3e4047
Erth <panot1997@hotmail.com>
2018-04-26 14:17:46 +0700

    Edit href

--- resources/views/createtripcompleted.blade.php
+++ resources/views/createtripcompleted.blade.php
@@ -3,11 +3,11 @@
 <div class="container">
     <h3 class="text-center register-header">Trip Created.</h3>
     <div class="row">
-        <div class="col-lg-2"></div>
-        <div class="col-lg-8">
-                <button type="button" class="btn btn-success" href="/">Back to home</button>
+        <div class="col-lg-5"></div>
+        <div class="col-lg-2">
+                <a href="/"><button type="button" class="btn btn-success">Back to home</button></a>
         </div>
-        <div class="col-lg-2"></div>
+        <div class="col-lg-5"></div>
     </div>
 </div>
 @endsection
