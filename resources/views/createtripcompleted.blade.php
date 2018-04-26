1c17e5c0777fc55fe1041ec1f2e6906fb2bf4f28
Erth <panot1997@hotmail.com>
2018-04-26 14:04:27 +0700

    Add new page

---
+++ resources/views/createtripcompleted.blade.php
@@ -0,0 +1,13 @@
+@extends('header')
+@section('page')
+<div class="container">
+    <h3 class="text-center register-header">Trip Created.</h3>
+    <div class="row">
+        <div class="col-lg-2"></div>
+        <div class="col-lg-8">
+                <button type="button" class="btn btn-success" href="/">Back to home</button>
+        </div>
+        <div class="col-lg-2"></div>
+    </div>
+</div>
+@endsection
