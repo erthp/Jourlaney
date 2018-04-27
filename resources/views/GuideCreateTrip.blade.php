10c6751e88a65a48eebc0bab05a3d114dc6b59c4
faiiptw <faii_ptw@hotmail.com>
2018-04-28 00:47:03 +0700

    Edit GuideCreateTrip

--- resources/views/GuideCreateTrip.blade.php
+++ resources/views/GuideCreateTrip.blade.php
@@ -4,13 +4,13 @@
             <div class="row">
                 <div class="col-4">
                     <div align="center">
-                        <a href = "recommend"><img src="../icon/medal.png" class="homemenu-icon" height="80" alt=""></a>
+                        <a href = "newfeeds"><img src="../icon/medal.png" class="homemenu-icon" height="80" alt=""></a>
                         <p class="homemenu-text">Recommend</p>
                     </div>
                 </div>
                 <div class="col-4">
                     <div align="center">
-                        <a href = "createtrip"><img src="../icon/bag.png" class="homemenu-icon" height="80" alt=""></a>
+                        <a href = "guidecreatetrip"><img src="../icon/bag.png" class="homemenu-icon" height="80" alt=""></a>
                         <p class="homemenu-text">Create Trip</p>
                     </div>
                 </div>
