4d2165fc1d4acd2961d6486a81aa4d0fb4de05f4
Erth <panot1997@hotmail.com>
2018-05-01 11:17:38 +0700

    Add password hashing

--- app/Http/Controllers/GuideRegisterController.php
+++ app/Http/Controllers/GuideRegisterController.php
@@ -12,7 +12,9 @@ class GuideRegisterController extends Controller
 {
     public function guideregis(Request $request){
         $username = $request->input('username');
-        $queryUser = DB::insert("insert into Users(username,userPassword,userFirstName,userLastName,userEmail,userGender,userDOB,userIdcard) values(?,?,?,?,?,?,?,?)",[$username,$request->input('password'),$request->input('firstname'),$request->input('lastname'),$request->input('email'),$request->input('gender'),$request->input('birthdate'),$request->input('idcard')]);
+        $password = $request->input('password');
+        $hash = hash('sha512',$password);
+        $queryUser = DB::insert("insert into Users(username,userPassword,userFirstName,userLastName,userEmail,userGender,userDOB,userIdcard) values(?,?,?,?,?,?,?,?)",[$username,$hash,$request->input('firstname'),$request->input('lastname'),$request->input('email'),$request->input('gender'),$request->input('birthdate'),$request->input('idcard')]);
         $queryGuide = DB::insert("insert into Guide(username,guideLicenseNumber) value(?,?)",[$request->input('username'),$request->input('guidelicense')]);
         return view('registercompleted');
     }
