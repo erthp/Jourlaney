685c0ba2acd371f472d1771ba2b037286740c11d
Erth <panot1997@hotmail.com>
2018-05-03 23:24:40 +0700

    Add image upload

--- app/Http/Controllers/GuideRegisterController.php
+++ app/Http/Controllers/GuideRegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Http\File;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\DB;
 use App\Models\User;
@@ -14,10 +15,20 @@ class GuideRegisterController extends Controller
         $username = $request->input('username');
         $password = $request->input('password');
         $hash = hash('sha512',$password);
-        $profilepic = $request->file('profilepic')->storeAs('/public/images/profilepic/', time().'.jpg');
-        $filename = time() . '.jpg';
-        $queryUser = DB::insert("insert into Users(username,userPassword,userFirstName,userLastName,userEmail,userGender,userDOB,userIdcard,userProfileImage) values(?,?,?,?,?,?,?,?,?)",[$username,$hash,$request->input('firstname'),$request->input('lastname'),$request->input('email'),$request->input('gender'),$request->input('birthdate'),$request->input('idcard'),$filename]);
-        $queryGuide = DB::insert("insert into Guide(username,guideLicenseNumber) value(?,?)",[$request->input('username'),$request->input('guidelicense')]);
+
+        // $image = $request->file('profilepic');
+        // $input['filename'] = time().'.'.$image->getClientOriginalExtension();
+        // $path = public_path('/images/profilepic');
+        // $image->move($path, $input['filename']);
+        // $profileImageName = $input['filename'];
+
+        $guideLicenseImage = $request->file('guidelicensepic');
+        $input['filename'] = time().'.'.$guideLicenseImage->getClientOriginalExtension();
+        $licensePath = public_path('/images/licensepic');
+        $guideLicenseImage->move($licensePath, $input['filename']);
+        $guideLicensePicName = $input['filename'];
+        $queryUser = DB::insert("insert into Users(username,userPassword,userFirstName,userLastName,userEmail,userGender,userDOB,userIdcard) values(?,?,?,?,?,?,?,?)",[$username,$hash,$request->input('firstname'),$request->input('lastname'),$request->input('email'),$request->input('gender'),$request->input('birthdate'),$request->input('idcard')]);
+        $queryGuide = DB::insert("insert into Guide(username,guideLicenseNumber,guideLicensePic) value(?,?,?)",[$request->input('username'),$request->input('guidelicense'),$guideLicensePicName]);
         return view('registercompleted');
     }
 
