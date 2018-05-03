31a3ea633aae8d0709679aa959f91e1b9a6059e2
Erth <panot1997@hotmail.com>
2018-05-04 00:04:30 +0700

    Edit username form

--- resources/views/Admin/admin.blade.php
+++ resources/views/Admin/admin.blade.php
@@ -21,16 +21,16 @@
                         <h3 class="panel-title">Please Sign In</h3>
                     </div>
                     <div class="panel-body">
-                        <form role="form">
+                        <form method="POST" action="{{ URL::to('/adminLogin') }}">
+                        {{ csrf_field() }}
                             <fieldset>
                                 <div class="form-group">
-                                    <input class="form-control" placeholder="E-mail" name="email" type="email" autofocus>
+                                    <input class="form-control" placeholder="Username" name="username" type="username" autofocus>
                                 </div>
                                 <div class="form-group">
                                     <input class="form-control" placeholder="Password" name="password" type="password" value="">
                                 </div>
-                                <!-- Change this to a button or input when using this as a form -->
-                                <a href="index.html" class="btn btn-lg btn-success btn-block">Login</a>
+                                <button type="submit" class="btn btn-primary btn-block">Login</button>
                             </fieldset>
                         </form>
                     </div>
