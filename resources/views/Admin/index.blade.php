98371c8bc707b14749dbf7b0e5a48426ba0e7757
Erth <panot1997@hotmail.com>
2018-05-04 00:25:43 +0700

    Add dashboard
    Admin is here!

--- resources/views/Admin/index.blade.php
+++ resources/views/Admin/index.blade.php
@@ -899,24 +899,13 @@
     <!-- /#wrapper -->
 
 
-    <script type="text/javascript" src="{{ URL::asset('js/jquery.mask.js') }}"></script>
-    <!-- jQuery -->
-    <script src="../vendor/jquery/jquery.min.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
-
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="../vendor/metisMenu/metisMenu.min.js"></script>
-
-    <!-- Morris Charts JavaScript -->
-    <script src="../vendor/raphael/raphael.min.js"></script>
-    <script src="../vendor/morrisjs/morris.min.js"></script>
-    <script src="../data/morris-data.js"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="../dist/js/sb-admin-2.js"></script>
-
+    <script type="text/javascript" src="{{ URL::asset('adminassets/jquery/jquery.min.js') }}"></script>
+    <script type="text/javascript" src="{{ URL::asset('adminassets/bootstrap/js/bootstrap.min.js') }}"></script>
+    <script type="text/javascript" src="{{ URL::asset('adminassets/metisMenu/metisMenu.min.js') }}"></script>
+    <script type="text/javascript" src="{{ URL::asset('adminassets/raphael/raphael.min.js') }}"></script>
+    <script type="text/javascript" src="{{ URL::asset('adminassets/jquery/morrisjs/morris.min.js') }}"></script>
+    <script type="text/javascript" src="{{ URL::asset('adminassets/data/morris-data.js') }}"></script>
+    <script type="text/javascript" src="{{ URL::asset('adminassets/dist/js/sb-admin-2.js') }}"></script>
 </body>
 
 </html>
