c8b37fb153c23244b3a0065fc1266570e3a3159f
Erth <panot1997@hotmail.com>
2018-05-03 14:43:29 +0700

    Add profile picture storing

--- app/Http/Controllers/GuideRegisterController.php
+++ app/Http/Controllers/GuideRegisterController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\DB;
-use App\Models\GuideRegister;
+use App\Models\User;
 
 class GuideRegisterController extends Controller
 {
@@ -14,8 +14,12 @@ class GuideRegisterController extends Controller
         $username = $request->input('username');
         $password = $request->input('password');
         $hash = hash('sha512',$password);
-        $queryUser = DB::insert("insert into Users(username,userPassword,userFirstName,userLastName,userEmail,userGender,userDOB,userIdcard) values(?,?,?,?,?,?,?,?)",[$username,$hash,$request->input('firstname'),$request->input('lastname'),$request->input('email'),$request->input('gender'),$request->input('birthdate'),$request->input('idcard')]);
+        $profilepic = $request->file('profilepic')->storeAs('/public/images/profilepic/', time().'.jpg');
+        $filename = time() . '.jpg';
+        $queryUser = DB::insert("insert into Users(username,userPassword,userFirstName,userLastName,userEmail,userGender,userDOB,userIdcard,userProfileImage) values(?,?,?,?,?,?,?,?,?)",[$username,$hash,$request->input('firstname'),$request->input('lastname'),$request->input('email'),$request->input('gender'),$request->input('birthdate'),$request->input('idcard'),$filename]);
         $queryGuide = DB::insert("insert into Guide(username,guideLicenseNumber) value(?,?)",[$request->input('username'),$request->input('guidelicense')]);
         return view('registercompleted');
     }
+
+
 }
