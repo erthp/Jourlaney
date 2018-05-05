80e542d495161bb72225ab885a314023b1035c79
faiiptw <faii_ptw@hotmail.com>
2018-05-05 14:14:36 +0700

    Edit form GuideCreateTrip

--- resources/views/GuideCreateTrip.blade.php
+++ resources/views/GuideCreateTrip.blade.php
@@ -37,16 +37,100 @@
                     </div>
                 </div>
             </div>
+            <br>
             <div class="row">
                 <div class="col-lg-2">
                     <label class="trip-label" for="location">Location</label>
                 </div>
-                <div class="col-lg-8">
+                <div class="col-lg-5">
                     <div class="form-group">
-                        <input type="text" class="form-control" name="location" id="location" data-parsley-required="true" data-parsley-type="alphanum">
+                    <select name="location" id="location">
+                        <option value="" selected>--------- เลือกจังหวัด ---------</option>
+                        <option value="กรุงเทพมหานคร">กรุงเทพมหานคร</option>
+                        <option value="กระบี่">กระบี่ </option>
+                        <option value="กาญจนบุรี">กาญจนบุรี </option>
+                        <option value="กาฬสินธุ์">กาฬสินธุ์ </option>
+                        <option value="กำแพงเพชร">กำแพงเพชร </option>
+                        <option value="ขอนแก่น">ขอนแก่น</option>
+                        <option value="จันทบุรี">จันทบุรี</option>
+                        <option value="ฉะเชิงเทรา">ฉะเชิงเทรา </option>
+                        <option value="ชัยนาท">ชัยนาท </option>
+                        <option value="ชัยภูมิ">ชัยภูมิ </option>
+                        <option value="ชุมพร">ชุมพร </option>
+                        <option value="ชลบุรี">ชลบุรี </option>
+                        <option value="เชียงใหม่">เชียงใหม่ </option>
+                        <option value="เชียงราย">เชียงราย </option>
+                        <option value="ตรัง">ตรัง </option>
+                        <option value="ตราด">ตราด </option>
+                        <option value="ตาก">ตาก </option>
+                        <option value="นครนายก">นครนายก </option>
+                        <option value="นครปฐม">นครปฐม </option>
+                        <option value="นครพนม">นครพนม </option>
+                        <option value="นครราชสีมา">นครราชสีมา </option>
+                        <option value="นครศรีธรรมราช">นครศรีธรรมราช </option>
+                        <option value="นครสวรรค์">นครสวรรค์ </option>
+                        <option value="นราธิวาส">นราธิวาส </option>
+                        <option value="น่าน">น่าน </option>
+                        <option value="นนทบุรี">นนทบุรี </option>
+                        <option value="บึงกาฬ">บึงกาฬ</option>
+                        <option value="บุรีรัมย์">บุรีรัมย์</option>
+                        <option value="ประจวบคีรีขันธ์">ประจวบคีรีขันธ์ </option>
+                        <option value="ปทุมธานี">ปทุมธานี </option>
+                        <option value="ปราจีนบุรี">ปราจีนบุรี </option>
+                        <option value="ปัตตานี">ปัตตานี </option>
+                        <option value="พะเยา">พะเยา </option>
+                        <option value="พระนครศรีอยุธยา">พระนครศรีอยุธยา </option>
+                        <option value="พังงา">พังงา </option>
+                        <option value="พิจิตร">พิจิตร </option>
+                        <option value="พิษณุโลก">พิษณุโลก </option>
+                        <option value="เพชรบุรี">เพชรบุรี </option>
+                        <option value="เพชรบูรณ์">เพชรบูรณ์ </option>
+                        <option value="แพร่">แพร่ </option>
+                        <option value="พัทลุง">พัทลุง </option>
+                        <option value="ภูเก็ต">ภูเก็ต </option>
+                        <option value="มหาสารคาม">มหาสารคาม </option>
+                        <option value="มุกดาหาร">มุกดาหาร </option>
+                        <option value="แม่ฮ่องสอน">แม่ฮ่องสอน </option>
+                        <option value="ยโสธร">ยโสธร </option>
+                        <option value="ยะลา">ยะลา </option>
+                        <option value="ร้อยเอ็ด">ร้อยเอ็ด </option>
+                        <option value="ระนอง">ระนอง </option>
+                        <option value="ระยอง">ระยอง </option>
+                        <option value="ราชบุรี">ราชบุรี</option>
+                        <option value="ลพบุรี">ลพบุรี </option>
+                        <option value="ลำปาง">ลำปาง </option>
+                        <option value="ลำพูน">ลำพูน </option>
+                        <option value="เลย">เลย </option>
+                        <option value="ศรีสะเกษ">ศรีสะเกษ</option>
+                        <option value="สกลนคร">สกลนคร</option>
+                        <option value="สงขลา">สงขลา </option>
+                        <option value="สมุทรสาคร">สมุทรสาคร </option>
+                        <option value="สมุทรปราการ">สมุทรปราการ </option>
+                        <option value="สมุทรสงคราม">สมุทรสงคราม </option>
+                        <option value="สระแก้ว">สระแก้ว </option>
+                        <option value="สระบุรี">สระบุรี </option>
+                        <option value="สิงห์บุรี">สิงห์บุรี </option>
+                        <option value="สุโขทัย">สุโขทัย </option>
+                        <option value="สุพรรณบุรี">สุพรรณบุรี </option>
+                        <option value="สุราษฎร์ธานี">สุราษฎร์ธานี </option>
+                        <option value="สุรินทร์">สุรินทร์ </option>
+                        <option value="สตูล">สตูล </option>
+                        <option value="หนองคาย">หนองคาย </option>
+                        <option value="หนองบัวลำภู">หนองบัวลำภู </option>
+                        <option value="อำนาจเจริญ">อำนาจเจริญ </option>
+                        <option value="อุดรธานี">อุดรธานี </option>
+                        <option value="อุตรดิตถ์">อุตรดิตถ์ </option>
+                        <option value="อุทัยธานี">อุทัยธานี </option>
+                        <option value="อุบลราชธานี">อุบลราชธานี</option>
+                        <option value="อ่างทอง">อ่างทอง </option>
+                        <option value="อื่นๆ">อื่นๆ</option>
+                    </select>
+                    &nbsp;&nbsp;
+                    <img src="../pic/add.png" hight="16px" width="16px">
                     </div>
                 </div>
             </div>
+            <br>
             <div class="row">
                 <div class="col-lg-2">
                     <label class="trip-label" for="date">Trip start</label>
@@ -63,19 +147,20 @@
                     </div>
                 </div>
             </div>
+            <br>
             <div class="row">
                 <div class="col-lg-2">
                 <label class="trip-label" for="trippic">Trip picture</label>
                 </div>
                 <div class="col-lg-10">
                     <div class="form-group">
-                        <a class='btn btn-primary' href='javascript:;'>
-                            Choose File...
+                        <a>
                             <input type="file" name="file_source" id="file_source" size="40" onchange='$("#upload-file-info").html($(this).val());'>
                         </a>
                     </div>
                 </div>
             </div>
+            <br>
             <div class="row">
                 <div class="col-lg-2">
                     <label class="trip-label" for="transportation">Transportation</label>
@@ -92,7 +177,7 @@
                 </div>
                 <div class="col-lg-2">
                     <div class="form-group">
-                        <input type="number" class="form-control" name="max-traveller" id="max-traveller"  data-parsley-required="true">
+                        <input type="number" class="form-control" name="max-traveller" id="max-traveller"  data-parsley-required="true"  min="1">
                     </div>
                 </div>
             </div>
@@ -132,7 +217,7 @@
                     <div class="form-group">
                     Day 1 <img src="../pic/add.png" hight="16px" width="16px"><br><br>
                         Time : <input type="time" class="" name="time" id="time">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                        <input type="text" class="" name="plan" id="plan"size="50">&nbsp;&nbsp;&nbsp;<img src="../pic/add.png" hight="16px" width="16px">
+                        <input type="text" class="" name="detail" id="detail"size="50">&nbsp;&nbsp;&nbsp;<img src="../pic/add.png" hight="16px" width="16px">
                         <input type="hidden" name="guideid" id="guideid" value="{{Session::get('guideid')}}">
                         <input type="hidden" name="_token" value="{{csrf_token()}}">
                     </div>
@@ -170,8 +255,8 @@
                     </table>
                 </form>
                 <br>
-                        <input type="hidden" name="guideid" id="guideid" value="{{Session::get('guideid')}}">
-                        <input type="hidden" name="_token" value="{{csrf_token()}}">
+                    <input type="hidden" name="guideid" id="guideid" value="{{Session::get('guideid')}}">
+                    <input type="hidden" name="_token" value="{{csrf_token()}}">
                     </div>
                 </div>
             </div>
