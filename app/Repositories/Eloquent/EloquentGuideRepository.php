b5a02f1e16672e3397531acb1306bc2649f52e5e
Erth <panot1997@hotmail.com>
2018-05-06 01:38:46 +0700

    Remove founction

--- app/Repositories/Eloquent/EloquentGuideRepository.php
+++ app/Repositories/Eloquent/EloquentGuideRepository.php
@@ -18,16 +18,6 @@ class EloquentGuideRepository implements GuideRepository
     }
 
     public static function guideRegister(Request $request){
-        $username = $request->input('username');
-        $password = $request->input('password');
-        $hash = hash('sha512',$password);
-
-        $guideLicenseImage = $request->file('guidelicensepic');
-        $input['filename'] = time().'.'.$guideLicenseImage->getClientOriginalExtension();
-        $licensePath = public_path('/images/licensepic');
-        $guideLicenseImage->move($licensePath, $input['filename']);
-        $guideLicensePicName = $input['filename'];
-        $queryUser = DB::insert("insert into Users(username,userPassword,userFirstName,userLastName,userEmail,userGender,userDOB,userIdcard) values(?,?,?,?,?,?,?,?)",[$username,$hash,$request->input('firstname'),$request->input('lastname'),$request->input('email'),$request->input('gender'),$request->input('birthdate'),$request->input('idcard')]);
-        $queryGuide = DB::insert("insert into Guide(username,guideLicenseNumber,guideLicensePic) value(?,?,?)",[$request->input('username'),$request->input('guidelicense'),$guideLicensePicName]);
+        
     }
 }
