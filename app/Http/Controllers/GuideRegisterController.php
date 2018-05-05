f174905e9dc63b918a6ab6a8c22f25a4ce09ccc4
Erth <panot1997@hotmail.com>
2018-05-06 01:54:41 +0700

    Add DB

--- app/Http/Controllers/GuideRegisterController.php
+++ app/Http/Controllers/GuideRegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Repositories\Eloquent\EloquentGuideRepository as guideRepo;
 
 class GuideRegisterController extends Controller
