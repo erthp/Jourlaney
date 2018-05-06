f61c8e4599e10c7baceb23e66040c276c00c60ac
Erth <panot1997@hotmail.com>
2018-05-07 00:13:55 +0700

    Add picture storing and password hashing

--- app/Http/Controllers/TouristRegisterController.php
+++ app/Http/Controllers/TouristRegisterController.php
@@ -12,8 +12,17 @@ class TouristRegisterController extends Controller
 {
     public function touristregis(Request $request){
         $username = $request->input('username');
-        $queryUser = DB::insert("insert into Users(username,userPassword,userFirstName,userLastName,userEmail,userGender,userDOB,userIdcard) values(?,?,?,?,?,?,?,?)",[$username,$request->input('password'),$request->input('firstname'),$request->input('lastname'),$request->input('email'),$request->input('gender'),$request->input('birthdate'),$request->input('idcard')]);
-        $queryTourist = DB::insert("insert into Tourist(username) value(?)",[$request->input('username')]);
+        $password = $request->input('password');
+        $hash = hash('sha512',$password);
+        
+        $touristIdCardImage = $request->file('touristidcardpic');
+        $input['filename'] = time().'.'.$touristIdCardImage->getClientOriginalExtension();
+        $picturePath = public_path('/images/idcardpic');
+        $touristIdCardImage->move($picturePath, $input['filename']);
+        $toursitIdCardPicName = $input['filename'];
+
+        $queryUser = DB::insert("insert into Users(username,userPassword,userFirstName,userLastName,userEmail,userGender,userDOB,userIdcard) values(?,?,?,?,?,?,?,?)",[$username,$hash,$request->input('firstname'),$request->input('lastname'),$request->input('email'),$request->input('gender'),$request->input('birthdate'),$request->input('idcard')]);
+        $queryTourist = DB::insert("insert into Tourist(username,touristIdCardPic) value(?,?)",[$request->input('username'),$toursitIdCardPicName]);
         return view('registercompleted');
     }
 }
