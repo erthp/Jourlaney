36100625971edf8a1f57dd112cb3e9d580203df0
Erth <panot1997@hotmail.com>
2018-05-10 00:06:17 +0700

    Add Function

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
+use Session;
 
 class ProfileController extends Controller
 {
-    //
+    public function showProfile($username){
+        $check = DB::table('Users')->where(['username'=>$username])->first();
+        $checkGuide = DB::table('Guide')->where(['username'=>$username])->first();
+        $checkTourist = DB::table('Tourist')->where(['username'=>$username])->first();
+
+        if(!empty($checkGuide)){
+            $profileGuideLocation = DB::table('Guide')->select('guideLocation')->where(['username'=>$username])->get();
+            Session::put('profileGuideLocation', $profileGuideLocation[0]->guideLocation);
+        }
+
+        if($check){
+            return view('Profile')->withUser($check,$checkGuide,$checkTourist);;
+        }
+    }
 }
