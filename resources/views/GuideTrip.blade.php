0b0b9b6a5bfd04edd233cda416139fda6fadbecb
Erth <panot1997@hotmail.com>
2018-05-11 01:40:19 +0700

    Add Trip name on title

--- resources/views/GuideTrip.blade.php
+++ resources/views/GuideTrip.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Jourlaney</title>
+    <title>{{ $trip -> tripName }} on Jourlaney</title>
     <link rel="shortcut icon" type="image/png" href="../favicon.png"/>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
         crossorigin="anonymous">
