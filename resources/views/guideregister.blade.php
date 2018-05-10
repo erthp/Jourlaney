2de4cfe8b3627e61be3cdfcceba669495efe5bbb
faiiptw <faii_ptw@hotmail.com>
2018-05-10 17:46:06 +0700

    Edit Register form

--- resources/views/guideregister.blade.php
+++ resources/views/guideregister.blade.php
@@ -2,12 +2,12 @@
 @section('page')
 <div class="container">
     <h3 class="text-center register-header">Guide Registeration</h3>
-        <form method="POST" id="register-form" name="register-form" action="{{URL::to('/guideregis')}}" enctype="multipart/form-data">
+        <form method="POST" id="register-form" name="register-form" action="{{URL::to('/guideregis')}}" enctype="multipart/form-data">    
             <div class="row mt-5">
                 <div class="col-lg-2">
                     <label class="register-label" for="username">Username</label>
                 </div>
-                <div class="col-lg-10">
+                <div class="col-lg-5">
                     <div class="form-group">
                         <input type="text" class="form-control" name="username" id="username" placeholder="Username" data-parsley-required="true" data-parsley-type="alphanum" data-parsley-length="[3, 20]">
                     </div>
@@ -17,7 +17,7 @@
                 <div class="col-lg-2">
                     <label class="register-label" for="password">Password</label>
                 </div>
-                <div class="col-lg-10">
+                <div class="col-lg-5">
                     <div class="form-group">
                         <input type="password" class="form-control" id="password" name="password" placeholder="Password" data-parsley-required="true" data-parsley-type="alphanum" data-parsley-length="[8, 40]" data-equalto="#repassword">
                     </div>
@@ -27,7 +27,7 @@
                 <div class="col-lg-2">
                 <label class="register-label" for="repassword">Re-password</label>
                 </div>
-                <div class="col-lg-10">
+                <div class="col-lg-5">
                     <div class="form-group">
                     <input type="password" class="form-control" id="repassword" name="repassword" placeholder="Re-password" data-parsley-required="true" data-parsley-type="alphanum" data-parsley-length="[8, 40]" data-equalto="#password">
                     </div>
@@ -37,7 +37,7 @@
                 <div class="col-lg-2">
                     <label class="register-label" for="firstname">First name</label>
                 </div>
-                <div class="col-lg-10">
+                <div class="col-lg-5">
                     <div class="form-group">
                         <input type="text" class="form-control" name="firstname" placeholder="First name" data-parsley-required="true" data-parsley-type="alphanum">
                     </div>
@@ -47,7 +47,7 @@
                 <div class="col-lg-2">
                     <label class="register-label" for="lastname">Last name</label>
                 </div>
-                <div class="col-lg-10">
+                <div class="col-lg-5">
                     <div class="form-group">
                         <input type="text" class="form-control" name="lastname" placeholder="Last name" data-parsley-required="true" data-parsley-type="alphanum">
                     </div>
@@ -57,7 +57,7 @@
                 <div class="col-lg-2">
                     <label class="register-label" for="email">Email</label>
                 </div>
-                <div class="col-lg-10">
+                <div class="col-lg-5">
                     <div class="form-group">
                         <input type="email" class="form-control" name="email" placeholder="Email" data-parsley-required="true" data-parsley-type="email">
                     </div>
@@ -67,7 +67,7 @@
                 <div class="col-lg-2">
                     <label class="register-label" for="gender">Gender</label>
                 </div>
-                <div class="col-lg-10">
+                <div class="col-lg-5">
                     <div class="form-group">
                         <div class="form-check form-check-inline">
                             <input class="form-check-input" type="radio" name="gender" id="gender" value="Male">
@@ -88,7 +88,7 @@
                 <div class="col-lg-2">
                     <label class="register-label" for="birthdate">Birthdate</label>
                 </div>
-                <div class="col-lg-10">
+                <div class="col-lg-5">
                     <div class="form-group">
                     <input type="date" class="form-control" name="birthdate" placeholder="Birthday" validateDate>
                     </div>
@@ -98,7 +98,7 @@
                 <div class="col-lg-2">
                     <label class="register-label" for="idcard">ID card number</label>
                 </div>
-                <div class="col-lg-10">
+                <div class="col-lg-5">
                     <div class="form-group">
                         <input type="text" class="form-control" id="idcard "name="idcard" placeholder="ID card number" data-parsley-type="integer" data-parsley-required="true" data-parsley-length="[13, 13]">
                     </div>
@@ -108,7 +108,7 @@
                 <div class="col-lg-2">
                     <label class="register-label" for="guidelicense">Guide license number</label>
                 </div>
-                <div class="col-lg-10">
+                <div class="col-lg-5">
                     <div class="form-group">
                         <input type="text" class="form-control" name="guidelicense" placeholder="Guide license number" data-parsley-type="integer" data-parsley-required="true" data-parsley-length="[7, 7]">
                         <input type="hidden" name="_token" value="{{csrf_token()}}">
@@ -119,17 +119,16 @@
                 <div class="col-lg-2">
                     <label class="register-label" for="guidelicensepic">Guide license card picture</label>
                 </div>
-                <div class="col-lg-10">
+                <div class="col-lg-5">
                     <div class="form-group">
                             <input type="file" name="guidelicensepic" id="guidelicensepic" required>
                         <div id="filename"></div>
                     </div>
                 </div>
             </div>
+            <br>
             <div class="row text-center mb-4">
-                <div class="col-lg-2">
-                </div>
-                <div class="col-lg-10">
+                <div class="col-lg-7">
                     <button type="submit" class="btn btn-success btn-block">Submit</button>
                 </div>
             </div>
