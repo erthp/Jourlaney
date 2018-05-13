99a63c573ac7965e6b555bd992173fdcfacaa5ce
Erth <panot1997@hotmail.com>
2018-05-14 00:02:39 +0700

    Edit function

--- app/Http/Controllers/TouristTripController.php
+++ app/Http/Controllers/TouristTripController.php
@@ -11,8 +11,27 @@ use App\Models\TouristTrip;
 class TouristTripController extends Controller
 {
     public function touristrip(Request $request){
-        //$username = $request->input('username');
-        $queryTouristTrip = DB::insert("insert into TouristTrip(tripName,tripLocation,tripDetails,tripDate,touristId) values(?,?,?,?,?)",[$request->input('tripname'),$request->input('location'),$request->input('date'),$request->input('file_source'),$request->input('transportation'),$request->input('max-traveller'),$request->input('language'),$request->input('trip-conditions'),$request->input('trip-details')]);
+        $lastTripId = DB::select("select tripId from TouristTrip order by tripId desc limit 1");
+        $tripIdObj = $lastTripId[0]->tripId;
+        $tripId = $tripIdObj+1;
+
+        $touristTripImage = $request->file('trippic');
+        $input['filename'] = time().'.'.$touristTripImage->getClientOriginalExtension();
+        $imagePath = public_path('/images/trippic');
+        $touristTripImage->move($imagePath, $input['filename']);
+        $touristTripPicName = $input['filename'];
+
+        $queryTouristTrip = DB::insert("insert into TouristTrip(tripId,tripName,tripStart,tripEnd,tripPicture,touristId) values(?,?,?,?,?,?)",[$tripId,$request->input('tripname'),$request->input('startdate'),$request->input('enddate'),$touristTripPicName,$request->input('touristid')]);
+        
+        if(!empty($request->input('location'))){
+            $queryLocation1 = DB::insert("insert into TouristTripLocation(tripId, tripLocation) value(?,?)",[$tripId,$request->input('location')]);
+            if(!empty($request->input('location2'))){
+                $queryLocation2 = DB::insert("insert into TouristTripLocation(tripId, tripLocation) value(?,?)",[$tripId,$request->input('location2')]);
+                if(!empty($request->input('location3'))){
+                    $queryLocation3 = DB::insert("insert into TouristTripLocation(tripId, tripLocation) value(?,?)",[$tripId,$request->input('location3')]);
+                }
+            }
+        }
         return view('createtripcompleted');
     }
 }
