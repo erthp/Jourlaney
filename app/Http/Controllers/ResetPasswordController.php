49c6b70a103e2246938f96863749031d56f51530
Erth <panot1997@hotmail.com>
2018-07-10 11:04:37 +0700

    Add function

--- app/Http/Controllers/ResetPasswordController.php
+++ app/Http/Controllers/ResetPasswordController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class ResetPasswordController extends Controller
 {
-    
+    public function resetPassword(Request $request){
+        $username = Session::get('username');
+        $oldPassword = $request->get('oldPassword');
+        $hashOldPassword = hash('sha512',$password);
+    }    
 }
