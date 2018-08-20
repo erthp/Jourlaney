d515e4af59f13a8a695dcdcea72b5773fecb3d9e
Erth <panot1997@hotmail.com>
2018-08-20 11:37:00 +0700

    Add search top nav

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,3 +1,20 @@
 @extends('headerNav')
 @section('page')
+<div class="container">
+    <div class="row searchpage-margin">
+        <div class="col-12">
+            <h4>Search</h4>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-8">
+            <form method="GET" action="{{URL::to('/search')}}">
+                <input type="text" class="searchbox-searchpage animated fadeIn" name="search" placeholder="Search trip">
+        </div>
+        <div class="col-4">
+                <button type="submit" class="btn btn-success btn-block searchpage-submitbutton">Submit</button>
+        </div>
+            </form>
+    </div>
+</div>
 @endsection
