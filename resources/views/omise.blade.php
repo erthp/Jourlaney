98db3f8c23a768e16643c98fdbdc93c83712b46d
Kasidis <knick6@hotmail.com>
2018-08-26 21:22:39 +0700

    Add action Path view

--- resources/views/omise.blade.php
+++ resources/views/omise.blade.php
@@ -2,7 +2,7 @@
 <head>
 </head>
 <body>
-<form name="checkoutForm" method="POST" action="{{URL::to('/checkout')}}">
+<form name="checkoutForm" method="POST" action="checkout">
   <script type="text/javascript" src="https://cdn.omise.co/omise.js"
     data-key="pkey_test_5d13mw1sktn0oad4nei"
     data-image="http://52.221.186.101/pic/add.png"
@@ -10,7 +10,7 @@
     data-button-label="Pay now"
     data-submit-label="Submit"
     data-location="no"
-    data-amount="10025"
+    data-amount="10025" 
     data-currency="thb"
     >
   </script>
