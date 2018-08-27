d25b6dcc2952a7a60af9d594b640fe2bc7e88e69
Erth <panot1997@hotmail.com>
2018-08-27 11:25:21 +0700

    Show trips in cards

--- resources/views/Profile.blade.php
+++ resources/views/Profile.blade.php
@@ -8,20 +8,37 @@
             <h4>Trip created by {{ $user -> userFirstName}}</h4>
             <br>
         </div>
-        @foreach($trip as $trip)
-        <div class ="col-4 text-center mb-4">
-            @if(!empty(Session::get('guideid')))
-            <a href="../GuideTrip/{{ $trip -> tripId }}">
-            @elseif(!empty(Session::get('touristid')))
-            <a href="../TouristTrip/{{ $trip -> tripId }}">
-            @endif
-                <img src="../images/trippic/{{ $trip -> tripPicture}}" class="img-responsive" height="150px">
-                <div class="mt-2">
-                    <h4>{{ $trip -> tripName }}</h4>
+        <div class="container">
+            <div class="row mt-4 animated fadeIn">
+            @foreach($trip as $trip)
+                @if(!empty(Session::get('guideid')))
+                <div class ="col-4 text-center mt-4">
+                    <div class="card hometrip-container" style="width: 20rem;">
+                        <img class="card-img-top hometrip-image" src="../images/trippic/{{ $trip -> tripPicture}}">
+                        <div class="image-overlay">
+                            <a class="image-overlay-text" href="../GuideTrip/{{ $trip -> tripId }}">VIEW TRIP</a>
+                        </div>
+                        <div class="card-body">
+                            <a href="../GuideTrip/{{ $trip -> tripId }}" class="hometrip-tripname">{{ $trip -> tripName }}</a>
+                        </div>
+                    </div>
                 </div>
-            </a>
+                @elseif(!empty(Session::get('touristid')))
+                <div class ="col-4 text-center mt-4">
+                    <div class="card hometrip-container" style="width: 20rem;">
+                        <img class="card-img-top hometrip-image" src="../images/trippic/{{ $trip -> tripPicture}}">
+                        <div class="image-overlay">
+                            <a class="image-overlay-text" href="../TouristTrip/{{ $trip -> tripId }}">VIEW TRIP</a>
+                        </div>
+                        <div class="card-body">
+                            <a href="../TouristTrip/{{ $trip -> tripId }}" class="hometrip-tripname">{{ $trip -> tripName }}</a>
+                        </div>
+                    </div>
+                </div>
+            @endif
+            @endforeach
+            </div>
         </div>
-        @endforeach
     </div>
 </div>
 
