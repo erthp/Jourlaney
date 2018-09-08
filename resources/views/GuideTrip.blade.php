4cc8a6b9199a9277231baf8338fc9a2512db8a0f
faiiptw <faii_ptw@hotmail.com>
2018-09-08 16:41:43 +0700

    Edit Calendar

--- resources/views/GuideTrip.blade.php
+++ resources/views/GuideTrip.blade.php
@@ -439,59 +439,8 @@
                                 <br><br><br>
                                 <!-- /.col-lg-6 (nested) -->
                                 <div class="col-lg-6">
-                                    <div class="month">      
-                                        <ul>
-                                          <li class="prev">&#10094;</li>
-                                          <li class="next">&#10095;</li>
-                                          <li>
-                                            May<br>
-                                            <span style="font-size:18px">2018</span>
-                                          </li>
-                                        </ul>
-                                    </div>
-                                    <ul class="weekdays">
-                                      <li>Mo</li>
-                                      <li>Tu</li>
-                                      <li>We</li>
-                                      <li>Th</li>
-                                      <li>Fr</li>
-                                      <li>Sa</li>
-                                      <li>Su</li>
-                                    </ul>
-                                    <ul class="days">
-                                      <li></li>
-                                      <li>1</li>
-                                      <li>2</li>
-                                      <li>3</li>
-                                      <li>4</li>
-                                      <li>5</li>
-                                      <li>6</li>
-                                      <li>7</li>
-                                      <li>8</li>
-                                      <li>9</li>
-                                      <li>10</li>
-                                      <li>11</li>
-                                      <li>12</li>
-                                      <li>13</li>
-                                      <li>14</li>
-                                      <li><span class="active">15</span></li>
-                                      <li>16</li>
-                                      <li>17</li>
-                                      <li>18</li>
-                                      <li>19</li>
-                                      <li>20</li>
-                                      <li>21</li>
-                                      <li>22</li>
-                                      <li>23</li>
-                                      <li>24</li>
-                                      <li>25</li>
-                                      <li>26</li>
-                                      <li>27</li>
-                                      <li>28</li>
-                                      <li>29</li>
-                                      <li>30</li>
-                                      <li>31</li>
-                                    </ul>
+                                <iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=th.th%23holiday%40group.v.calendar.google.com&amp;color=%23125A12&amp;ctz=Asia%2FBangkok" style="border-width:0" width="600" height="400" frameborder="0" scrolling="no"></iframe>
+                                </div>
                                 <!-- /.col-lg-6 (nested) -->
                             </div>
                             <!-- /.row (nested) -->
