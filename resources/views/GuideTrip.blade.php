e6e84517f2f44d4a5f61949a1064489b464b05c7
Erth <panot1997@hotmail.com>
2018-09-11 11:56:26 +0700

    Add js

--- resources/views/GuideTrip.blade.php
+++ resources/views/GuideTrip.blade.php
@@ -362,6 +362,8 @@
                                 <br><br><br>
                                 <!-- /.col-lg-6 (nested) -->
                                 <div class="col-lg-6">
+                                <input type="hidden" id="tripStart" value="{{ $trip -> tripStart }}">
+                                <input type="hidden" id="tripEnd" value="{{ $trip -> tripEnd }}">
                                 <div class="calendar"></div>
 
                                 <!-- <iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=th.th%23holiday%40group.v.calendar.google.com&amp;color=%23125A12&amp;ctz=Asia%2FBangkok" style="border-width:0" width="600" height="400" frameborder="0" scrolling="no"></iframe> -->
@@ -384,7 +386,10 @@
     </div>
     <script>
 $(function() {
-    $('.calendar').pignoseCalendar();
+    $('.calendar').pignoseCalendar({
+        minDate: document.getElementById('tripStart').value,
+        maxDate: document.getElementById('tripEnd').value
+    });
 });
 </script>
 </body>
