b64036e0c0adb00ca1dc6ae0f1b3d080679c0b38
Erth <panot1997@hotmail.com>
2018-09-11 12:08:19 +0700

    Add route

--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,7 @@ Route::get('headerNav',function(){
 });
 
 Route::get('Profile/{username}', 'ProfileController@showProfile')->name('Profile.showProfile');
+Route::get('Profile/{username}/FreeDay', 'ProfileController@showProfileFreeDay')->name('Profile.showProfileFreeDay');
 Route::get('GuideTrip/{tripId}', 'TripController@showGuideTrip')->name('GuideTrip.ShowTrip');
 Route::get('guideShowEditTrip/{tripId}', 'TripController@guideShowEditTrip')->name('GuideShowEditTrip.ShowEditTrip');
 Route::get('guideShowEditTripDetails/{tripId}', 'TripController@guideShowEditTripDetails')->name('GuideShowEditTripDetails.ShowEditTripDetails');
