175e5c5862686f36316356c45e0e21f4b9a52488
Erth <panot1997@hotmail.com>
2018-09-11 22:21:48 +0700

    Edit function

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -12,22 +12,26 @@ class ProfileController extends Controller
         $check = DB::table('Users')->where(['username'=>$username])->first();
         $checkGuide = DB::table('Guide')->where(['username'=>$username])->first();
         $checkTourist = DB::table('Tourist')->where(['username'=>$username])->first();
+        $touristRating = 0;
+        $guideRating = 0;
+        $guideLocation = "";
 
         if(!empty($checkGuide)){
             $profileGuideLocation = DB::table('Guide')->select('guideLocation')->where(['username'=>$username])->get();
-            Session::put('profileGuideLocation', $profileGuideLocation[0]->guideLocation);
+            $guideLocation = $profileGuideLocation[0]->guideLocation;
             $profileGuideRating = DB::table('Guide')->select('guideRating')->where(['username'=>$username])->get();
-            Session::put('profileGuideRating', $profileGuideRating[0]->guideRating);
+            $guideRating = $profileGuideRating[0]->guideRating;
             $trip = DB::select('select * from GuideTrip join Guide on GuideTrip.guideId=Guide.guideId where Guide.username="'.$username.'"');
         }
         if(!empty($checkTourist)){
             $profileTouristRating = DB::table('Tourist')->select('touristRating')->where(['username'=>$username])->get();
+            $touristRating = $profileTouristRating[0]->touristRating;
             $trip = DB::select('select * from TouristTrip join Tourist on TouristTrip.touristId=Tourist.touristId where Tourist.username="'.$username.'"');
         }
 
         if($check){
             $userProfileImage = DB::table('Users')->select('userProfileImage')->where(['username'=>$username])->get();
-            return view('Profile', ['trip' => $trip])->withUser($check,$checkGuide,$checkTourist);
+            return view('Profile', ['trip' => $trip])->withUser($check,$checkGuide,$checkTourist)->with('guideLocation',$guideLocation)->with('guideRating',$guideRating)->with('touristRating',$touristRating);
         }
     }
 
