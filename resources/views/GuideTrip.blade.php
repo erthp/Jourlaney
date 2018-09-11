cf86f3a1868e5eab10a8a12cf5805ed5eba94a0f
Erth <panot1997@hotmail.com>
2018-09-11 11:29:37 +0700

    Add calendar cores

--- resources/views/GuideTrip.blade.php
+++ resources/views/GuideTrip.blade.php
@@ -7,7 +7,7 @@
     <link rel="shortcut icon" type="image/png" href="../favicon.png"/>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
         crossorigin="anonymous">
-    <link href="{{ URL::asset('css/pignose.calendar.css') }}" rel="stylesheet">
+    <link href="{{ URL::asset('pg-calendar/dist/css/pignose.calendar.min.css') }}" rel="stylesheet">
     <link href="{{ URL::asset('css/jourlaney.css') }}" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
         crossorigin="anonymous"></script>
@@ -16,9 +16,9 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
         crossorigin="anonymous"></script>
     <script type="text/javascript" src="{{ URL::asset('js/jquery.mask.js') }}"></script>
-    <script type="text/javascript" src="{{ URL::asset('js/pignose.calendar.js') }}"></script>
     <script type="text/javascript" src="{{ URL::asset('js/moment.min.js') }}"></script>
     <script type="text/javascript" src="{{ URL::asset('js/parsley.js') }}"></script>
+    <script type="text/javascript" src="{{ URL::asset('pg-calendar/dist/js/pignose.calendar.min.js') }}"></script>
     <script type="text/javascript" src="{{ URL::asset('js/jourlaney.js') }}"></script>
     <style>
         .masthead{
@@ -382,10 +382,11 @@
   
     @yield('page')
     </div>
-</body>
-<script>
+    <script>
 $(function() {
     $('.calendar').pignoseCalendar();
 });
 </script>
+</body>
+
 </html>
