b2b3c841b471529d766a134ca9d5ba44819eceea
faiiptw <faii_ptw@hotmail.com>
2018-09-18 14:39:36 +0700

    Edit Search

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -37,8 +37,8 @@
     <div class="row">
         <div class="col-1"></div>
         <div class="col-8">
-            <form method="GET" action="{{URL::to('search')}}">
-               <input type="text" class="searchbox-searchpage animated fadeIn" id="name" name="name" placeholder="Trip name">   
+            <form method="GET" action="{{URL::to('/search')}}">
+               <input type="text" class="searchbox-searchpage animated fadeIn" name="name" placeholder="Trip name">   
         </div>
         <div class="col-2">
                 <button type="submit" class="btn btn-info btn-block searchpage-submitbutton"><img src="../pic/search.png" height="24"></button>
@@ -51,15 +51,15 @@
         <div class="col-4">
             <div class="form-group">
                 <label class="search-datelabel">Start date</label>
-                <input type="date" class="form-control" name="startdate" id="startdate" data-parsley-required="true" data-parsley-type="alphanum">
+                <input type="date" class="form-control" name="startdate" id="startdate" data-parsley-required="true" data-parsley-type="alphanum" required>
             </div>
         </div>
         <div class="col-4">
             <div class="form-group">
                 <label class="search-datelabel">End date</label>
-                <input type="date" class="form-control" name="enddate" id="enddate" data-parsley-required="true" data-parsley-type="alphanum">
+                <input type="date" class="form-control" name="enddate" id="enddate" data-parsley-required="true" data-parsley-type="alphanum" required>
             </div>
-        </div>
+        </div>  
         <div class="col-2">
                 <br><br><button type="submit" class="btn btn-info btn-block searchpage-submitbutton"><img src="../pic/search.png" height="24"></button>
         </div>
@@ -76,7 +76,7 @@
                     <div class="card hometrip-container" style="width: 20rem;">
                         <img class="card-img-top hometrip-image" src="../images/trippic/{{ $tripresult -> tripPicture }}">
                         <div class="image-overlay">
-                            <a class="image-overlay-text" href="../GuideTrip/{{ $tripresult -> tripId }}">VIEW TRIP</a>
+                            <a class="image-overlay-text" href="../GuideTrip/19">VIEW TRIP</a>
                         </div>
                         <div class="card-body">
                             <a href="../GuideTrip/{{ $tripresult -> tripId }}" class="hometrip-tripname">{{ $tripresult -> tripName }}</a>
