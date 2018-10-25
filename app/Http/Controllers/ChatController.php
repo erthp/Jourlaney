a8d2b179839eaec348003d586050bd88a83491fb
Erth <panot1997@hotmail.com>
2018-10-25 16:24:57 +0700

    Add order status

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -16,8 +16,9 @@ class ChatController extends Controller
         $touristId = $request->input('touristId');
         $guideId = $request->input('guideId');
         $guideTripId = $request->input('tripId');
-        $create = DB::insert("insert into ChatRoom(chatRoomId, guideId, touristId, guideTripId) values(?,?,?,?)",[$chatId,$guideId,$touristId,$guideTripId]);
-        
+        $createChat = DB::insert("insert into ChatRoom(chatRoomId, guideId, touristId, guideTripId) values(?,?,?,?)",[$chatId,$guideId,$touristId,$guideTripId]);
+        $createOrder = DB::insert("insert into Order(chatRoomId, status values(?,?)",[$chatId,"Chat"]);
+
         $touristId = Session::get('touristid');
         $chatList = DB::select("select * from ChatRoom c join Guide g on c.guideId=g.guideId join Tourist t on c.touristId=t.touristId join Users gu on g.username=gu.username where c.touristId=".$touristId." group by c.chatRoomId desc");
         $maxChatRoomId = DB::select("select MAX(c.chatRoomId) as chatRoomId from ChatRoom c join Tourist t on c.touristId=t.touristId where t.touristId=".$touristId);
@@ -34,6 +35,7 @@ class ChatController extends Controller
             $maxChatRoomId = DB::select("select MAX(c.chatRoomId) as chatRoomId from ChatRoom c join Guide g on c.guideId=g.guideId where g.guideId=".$guideId);
             $chatRoomId = $maxChatRoomId[0]->chatRoomId;
             $query = DB::select("select * from ChatRoom c join Guide g on c.guideId=g.guideId join Tourist t on c.touristId=t.touristId join GuideTrip gt on c.guideTripId=gt.tripId join Users u on t.username=u.username where c.chatRoomId =".$chatRoomId);
+            $orderStatus = DB::select("select * from TripOrder where chatRoomId =".$chatRoomId);
             }else{
                 return view('404');
             }
@@ -44,7 +46,8 @@ class ChatController extends Controller
                 $maxChatRoomId = DB::select("select MAX(c.chatRoomId) as chatRoomId from ChatRoom c join Tourist t on c.touristId=t.touristId where t.touristId=".$touristId);
                 $chatRoomId = $maxChatRoomId[0]->chatRoomId;
                 $query = DB::select("select * from ChatRoom c join Guide g on c.guideId=g.guideId join Tourist t on c.touristId=t.touristId join GuideTrip gt on c.guideTripId=gt.tripId join Users u on g.username=u.username where c.chatRoomId =".$chatRoomId);
-                //dd($chatList);
+                $orderStatus = DB::select("select * from TripOrder where chatRoomId =".$chatRoomId);
+                //dd($orderStatus);
             }else{
                 return view('404');
             }
@@ -52,7 +55,7 @@ class ChatController extends Controller
             return view('404');
         }
         
-        return view('chat',['query' => $query])->with('chatList',$chatList)->with('chatRoomId',$chatRoomId);
+        return view('chat',['query' => $query])->with('chatList',$chatList)->with('chatRoomId',$chatRoomId)->with('orderStatus',$orderStatus);
     }
 
     public function ShowChat($chatRoomId){
@@ -60,15 +63,17 @@ class ChatController extends Controller
             $query = DB::select("select * from ChatRoom c join Guide g on c.guideId=g.guideId join Tourist t on c.touristId=t.touristId join GuideTrip gt on c.guideTripId=gt.tripId join Users u on t.username=u.username where c.chatRoomId =".$chatRoomId);
             $guideId = Session::get('guideid');
             $chatList = DB::select("select * from ChatRoom c join Guide g on c.guideId=g.guideId join Tourist t on c.touristId=t.touristId join Users tu on t.username=tu.username where c.guideId=".$guideId." group by c.chatRoomId desc");
+            $orderStatus = DB::select("select * from TripOrder where chatRoomId =".$chatRoomId);
         }elseif(Session::get('touristid')){
             $query = DB::select("select * from ChatRoom c join Guide g on c.guideId=g.guideId join Tourist t on c.touristId=t.touristId join GuideTrip gt on c.guideTripId=gt.tripId join Users u on g.username=u.username where c.chatRoomId =".$chatRoomId);
             $touristId = Session::get('touristid');
             $chatList = DB::select("select * from ChatRoom c join Guide g on c.guideId=g.guideId join Tourist t on c.touristId=t.touristId join Users gu on g.username=gu.username where c.touristId=".$touristId." group by c.chatRoomId desc");
+            $orderStatus = DB::select("select * from TripOrder where chatRoomId =".$chatRoomId);
         }else{
             return view('404');
         }
         dd($query);
-        return view('chat',['query' => $query])->with('chatList',$chatList)->with('chatRoomId',$chatRoomId);
+        return view('chat',['query' => $query])->with('chatList',$chatList)->with('chatRoomId',$chatRoomId)->with('orderStatus',$orderStatus);
     }
 
     public function SendChat(Request $request){
@@ -82,8 +87,9 @@ class ChatController extends Controller
         
             $query = DB::select("select * from ChatRoom c join Guide g on c.guideId=g.guideId join Tourist t on c.touristId=t.touristId join GuideTrip gt on c.guideTripId=gt.tripId join Users u on t.username=u.username where c.chatRoomId =".$chatRoomId);
             $chatList = DB::select("select * from ChatRoom c join Guide g on c.guideId=g.guideId join Tourist t on c.touristId=t.touristId join Users tu on t.username=tu.username where c.guideId=".$guideId." group by c.chatRoomId desc");
-        
-            return view('chat',['query' => $query])->with('chatList',$chatList)->with('chatRoomId',$chatRoomId);
+            $orderStatus = DB::select("select * from TripOrder where chatRoomId =".$chatRoomId);
+
+            return view('chat',['query' => $query])->with('chatList',$chatList)->with('chatRoomId',$chatRoomId)->with('orderStatus',$orderStatus);
         }elseif(Session::get('touristid')){
             $touristId = Session::get('touristid');
             $message = $request->input('msg');
@@ -91,8 +97,9 @@ class ChatController extends Controller
 
             $query = DB::select("select * from ChatRoom c join Guide g on c.guideId=g.guideId join Tourist t on c.touristId=t.touristId join GuideTrip gt on c.guideTripId=gt.tripId join Users u on g.username=u.username where c.chatRoomId =".$chatRoomId);
             $chatList = DB::select("select * from ChatRoom c join Guide g on c.guideId=g.guideId join Tourist t on c.touristId=t.touristId join Users gu on g.username=gu.username where c.touristId=".$touristId." group by c.chatRoomId desc");
-        
-            return view('chat',['query' => $query])->with('chatList',$chatList)->with('chatRoomId',$chatRoomId);
+            $orderStatus = DB::select("select * from TripOrder where chatRoomId =".$chatRoomId);
+
+            return view('chat',['query' => $query])->with('chatList',$chatList)->with('chatRoomId',$chatRoomId)->with('orderStatus',$orderStatus);
         }else{
             return view('404');
         }
