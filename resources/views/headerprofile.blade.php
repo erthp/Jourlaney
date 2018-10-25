f2c07671e48fbb8485ab9e13b04b90992fb366fd
faiiptw <faii_ptw@hotmail.com>
2018-10-26 02:03:33 +0700

    Edit Icon

--- resources/views/headerprofile.blade.php
+++ resources/views/headerprofile.blade.php
@@ -38,64 +38,7 @@
                 @if(!empty(Session::get('username')))
                 <div class="navbar-header">
                     <ul class="nav navbar-top-links navbar-right">
-                        <li class="dropdown">
-                    		<a class="dropdown" data-toggle="dropdown" href="#">
-                        	    <img src="../pic/bell.png" class="mr-3">
-                            </a>
-                            <ul class="dropdown-menu dropdown-alerts">
-                                    <!-- <li>
-                                        <a href="#">
-                                            <div>
-                                                <i class="fa fa-comment fa-fw"></i> New Comment
-                                                <span class="pull-right text-muted small">4 minutes ago</span>
-                                            </div>
-                                        </a>
-                                    </li>
-                                    <li class="divider"></li>
-                                    <li>
-                                        <a href="#">
-                                            <div>
-                                                <i class="fa fa-twitter fa-fw"></i> 3 New Followers
-                                                <span class="pull-right text-muted small">12 minutes ago</span>
-                                            </div>
-                                        </a>
-                                    </li>
-                                    <li class="divider"></li>
-                                    <li>
-                                        <a href="#">
-                                            <div>
-                                                <i class="fa fa-envelope fa-fw"></i> Message Sent
-                                                <span class="pull-right text-muted small">4 minutes ago</span>
-                                            </div>
-                                        </a>
-                                    </li>
-                                    <li class="divider"></li>
-                                    <li>
-                                        <a href="#">
-                                            <div>
-                                                <i class="fa fa-tasks fa-fw"></i> New Task
-                                                <span class="pull-right text-muted small">4 minutes ago</span>
-                                            </div>
-                                        </a>
-                                    </li>
-                                    <li class="divider"></li>
-                                    <li>
-                                        <a href="#">
-                                            <div>
-                                                <i class="fa fa-upload fa-fw"></i> Server Rebooted
-                                                <span class="pull-right text-muted small">4 minutes ago</span>
-                                            </div>
-                                        </a>
-                                    </li>
-                                    <li class="divider"></li>
-                                    <li>
-                                        <a class="text-center" href="#">
-                                            <strong>See All Alerts</strong>
-                                            <i class="fa fa-angle-right"></i>
-                                        </a>
-                                    </li> -->
-                            </ul>
-                        </li>
+                        <a href = "{{ URL::to('/') }}"><img src="../pic/uk.png" class="mr-3"></a>
                             <!-- dropdown-alerts -->
                         <a href = "{{ URL::to('/ShowChatPage') }}"><img src="../pic/chat.png" class="mr-3"></a>
                             <!-- dropdown-alerts -->
