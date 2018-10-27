43163bed10f5b2a7a08cbbe1411f6ea76d3a7c81
Erth <panot1997@hotmail.com>
2018-10-27 17:50:09 +0700

    Add function

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use DB;
 
 class OrderController extends Controller
 {
-    //
+    public function CreateOrder(Request $request){
+    
+        $chatRoomId = $request->input('chatRoomId');
+        $agreementDetails = $request->input('tripDetails');
+        $tripStartDate = $request->input('startDate');
+        $tripCost = $request->input('tripCost');
+        $tripCostWithVat = ($tripCost * 83) / 100;
+
+        $query = DB::update('update TripOrder set status = ?, agreementDetails = ?, tripStartDate = ?, tripCost = ?, tripCostWithVat = ? where chatRoomId = ?',["Transfer",$agreementDetails,$tripStartDate,$tripCost,$tripCostWithVat,$chatRoomId]);
+        return app('App\Http\Controllers\ChatController')->ShowChatPage();
+    }
 }
