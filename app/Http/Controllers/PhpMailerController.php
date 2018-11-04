a6968ecd78eb15ffef41677c62252d7bcf47487c
Kasidis <knick6@hotmail.com>
2018-11-04 21:21:23 +0700

    Add Phpmailer Controller

---
+++ app/Http/Controllers/PhpMailerController.php
@@ -0,0 +1,53 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+		
+namespace App\Http\Controllers;
+date_default_timezone_set('Asia/Bangkok');
+require 'PHPMailer/PHPMailerAutoload.php';
+class PhpMailerController extends Controller{  
+    public function Mail(Request $request){
+		// Import PHPMailer classes into the global namespace
+		// These must be at the top of your script, not inside a function
+		
+		//Load Composer's autoloader
+		$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+		try {
+			//Server settings
+			$mail->SMTPDebug = 2;                                 // Enable verbose debug output
+			$mail->isSMTP();                                      // Set mailer to use SMTP
+			$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+			$mail->SMTPAuth = true;                               // Enable SMTP authentication
+			$mail->Username = '่jourlaney@gmail.com';                 // SMTP username
+			$mail->Password = 'jourlaney123';                           // SMTP password
+			$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+			$mail->Port = 587;                                    // TCP port to connect to
+		
+			//Recipients
+			$mail->setFrom('knick6@otmail.com', 'Knack');
+			$mail->addAddress('kasidis.sr@mail.kmutt.ac.th', 'Kasidis Sri');     // Add a recipient
+			//$mail->addAddress('ellen@example.com');               // Name is optional  
+			//$mail->addReplyTo('info@example.com', 'Information');
+			//$mail->addCC('cc@example.com');
+			//$mail->addBCC('bcc@example.com');
+		
+			//Attachments
+			//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+			//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+		
+			//Content
+			$mail->isHTML(true);                                  // Set email format to HTML
+			$mail->Subject = 'Here is the subject';
+			$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+			$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+		
+			$mail->send();
+			echo 'Message has been sent';
+			} catch (Exception $e) {
+			echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+		}
+}
+}
+
+?>
