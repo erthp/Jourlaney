5e9e5e67445f7d39b3d57734fcbc0fd5e40195b8
Erth <panot1997@hotmail.com>
2018-11-12 16:15:55 +0700

    Hiding bank account form

--- app/Http/Controllers/GuideRegisterController.php
+++ app/Http/Controllers/GuideRegisterController.php
@@ -29,9 +29,8 @@ class GuideRegisterController extends Controller
                 $queryUser = DB::insert("insert into Users(username,userPassword,userFirstName,userLastName,userEmail,userGender,userDOB,userIdcard) values(?,?,?,?,?,?,?,?)",[$username,$hash,$request->input('firstname'),$request->input('lastname'),$request->input('email'),$request->input('gender'),$request->input('birthdate'),$request->input('idcard')]);
                 $queryGuide = DB::insert("insert into Guide(username,guideLicenseNumber,guideLicensePic) value(?,?,?)",[$request->input('username'),$request->input('guidelicense'),$guideLicensePicName]);
                 
-                $getGuideId = DB::select("select guideId from Guide where username=".$username);
-                $guideId = $getGuideId[0]->guideId;
-                $queryBankAccount = DB::insert("insert into GuideBankAccount(guideId,bankAccountNumber,bankAccountBank) value(?,?,?)",[$guideId,$request->input('accountno'),$request->input('bankname')]);
+                // $guideId = $getGuideId[0]->guideId;
+                // $queryBankAccount = DB::insert("insert into GuideBankAccount(guideId,bankAccountNumber,bankAccountBank) value(?,?,?)",[$guideId,$request->input('accountno'),$request->input('bankname')]);
                 return view('registercompleted');
             }
             else{
