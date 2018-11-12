ef4f43879921651a0e0e9661db1e305bdce3100e
Erth <panot1997@hotmail.com>
2018-11-12 16:15:37 +0700

    Add email sending

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use DB;
 use Session;
+use PHPMailer;
+use PHPMailer\Exception;
 
 class AdminController extends Controller
 {
@@ -56,7 +58,253 @@ class AdminController extends Controller
     public function guideverify(Request $req){
         $username = $req->get('username');
         $queryVerify = DB::update('update Guide set guideVerification = ? where username = ?',[1,$username]);
+
+        $queryGuide = DB::select('select * from Users where username = ?',[$username]);
+        $guideFirstName = $queryGuide[0]->userFirstName;
+        $guideEmail = $queryGuide[0]->userEmail;
+        //Load Composer's autoloader
+        require 'PHPMailer/PHPMailerAutoload.php';
+
+        $mail = new PHPMailer();                              // Passing `true` enables exceptions
+        try {
+            //Server settings
+            $mail->SMTPDebug = 0;                                 // Enable verbose debug output
+            $mail->isSMTP();                                      // Set mailer to use SMTP
+            $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+            $mail->SMTPAuth = true;                               // Enable SMTP authentication
+            $mail->Username = env('MAIL_USERNAME');                 // SMTP username
+            $mail->Password = env('MAIL_PASSWORD');                           // SMTP password
+            $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+            $mail->Port = 587;                                    // TCP port to connect to
+
+            //Recipients
+            $mail->setFrom('jourlaney@gmail.com', 'Jourlaney');
+            $mail->addAddress($guideEmail, $guideFirstName);     // Add a recipient
+            //$mail->addAddress('ellen@example.com');               // Name is optional
+            //$mail->addReplyTo('info@example.com', 'Information');
+            //$mail->addCC('cc@example.com');
+            //$mail->addBCC('bcc@example.com');
+
+            //Attachments
+            //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+            //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+            //Content
+            $mail->isHTML(true);                                  // Set email format to HTML
+            $mail->Subject = 'Your account has verified!';
+            $mail->Body    = '<!DOCTYPE html>
+            <html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
+            <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width"> 
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="x-apple-disable-message-reformatting">
+                <title>Jourlaney Quotation</title>
+                <style>
+                    html,
+                    body {
+                        margin: 0 auto !important;
+                        padding: 0 !important;
+                        height: 100% !important;
+                        width: 100% !important;
+                    }
+            
+                    * {
+                        -ms-text-size-adjust: 100%;
+                        -webkit-text-size-adjust: 100%;
+                    }
+            
+            
+                    div[style*="margin: 16px 0"] {
+                        margin: 0 !important;
+                    }
+            
+            
+                    table,
+                    td {
+                        mso-table-lspace: 0pt !important;
+                        mso-table-rspace: 0pt !important;
+                    }
+            
+                    
+                    table {
+                        border-spacing: 0 !important;
+                        border-collapse: collapse !important;
+                        table-layout: fixed !important;
+                        margin: 0 auto !important;
+                    }
+                    table table table {
+                        table-layout: auto;
+                    }
+            
+                    
+                    a {
+                        text-decoration: none;
+                    }
+            
+            
+                    img {
+                        -ms-interpolation-mode:bicubic;
+                    }
+            
+            
+                    *[x-apple-data-detectors],  /* iOS */
+                    .unstyle-auto-detected-links *,
+                    .aBn {
+                        border-bottom: 0 !important;
+                        cursor: default !important;
+                        color: inherit !important;
+                        text-decoration: none !important;
+                        font-size: inherit !important;
+                        font-family: inherit !important;
+                        font-weight: inherit !important;
+                        line-height: inherit !important;
+                    }
+            
+            
+                    .a6S {
+                    display: none !important;
+                    opacity: 0.01 !important;
+                }
+            
+                img.g-img + div {
+                    display: none !important;
+                }
+            
+                    
+                    @media only screen and (min-device-width: 320px) and (max-device-width: 374px) {
+                        u ~ div .email-container {
+                            min-width: 320px !important;
+                        }
+                    }
+            
+                    @media only screen and (min-device-width: 375px) and (max-device-width: 413px) {
+                        u ~ div .email-container {
+                            min-width: 375px !important;
+                        }
+                    }
+            
+                    @media only screen and (min-device-width: 414px) {
+                        u ~ div .email-container {
+                            min-width: 414px !important;
+                        }
+                    }
+            
+                </style>
+                
+                <style>
+            
+                
+                    .button-td,
+                    .button-a {
+                        transition: all 100ms ease-in;
+                    }
+                    .button-td-primary:hover,
+                    .button-a-primary:hover {
+                        background: #555555 !important;
+                        border-color: #555555 !important;
+                    }
+            
+                    /* Media Queries */
+                    @media screen and (max-width: 600px) {
+            
+                        .email-container {
+                            width: 100% !important;
+                            margin: auto !important;
+                        }
+            
+            
+                        .fluid {
+                            max-width: 100% !important;
+                            height: auto !important;
+                            margin-left: auto !important;
+                            margin-right: auto !important;
+                        }
+            
+            
+                        .stack-column,
+                        .stack-column-center {
+                            display: block !important;
+                            width: 100% !important;
+                            max-width: 100% !important;
+                            direction: ltr !important;
+                        }
+            
+                        .stack-column-center {
+                            text-align: center !important;
+                        }
+            
+            
+                        .center-on-narrow {
+                            text-align: center !important;
+                            display: block !important;
+                            margin-left: auto !important;
+                            margin-right: auto !important;
+                            float: none !important;
+                        }
+                        table.center-on-narrow {
+                            display: inline-block !important;
+                        }
+            
+            
+                        .email-container p {
+                            font-size: 17px !important;
+                        }
+                    }
+            
+                </style>
+            
+            
+            </head>
+            
+            <body width="100%" style="margin: 0; padding: 0 !important; mso-line-height-rule: exactly; background-color: #c0dfd9;">
+                <center style="width: 100%; background-color: #c0dfd9;">
+                    <div style="display: none; font-size: 1px; line-height: 1px; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden; mso-hide: all; font-family: sans-serif;">
+                    </div>
+                    <div style="display: none; font-size: 1px; line-height: 1px; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden; mso-hide: all; font-family: sans-serif;">
+                        &zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;&zwnj;&nbsp;
+                    </div>
+            
+                    <table align="center" role="presentation" cellspacing="0" cellpadding="0" border="0" width="600" style="margin: 0 auto;" class="email-container">
+                        <tr>
+                            <td style="background-color: #ffffff;">
+                                <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
+                                    <tr>
+                                        <td style="padding: 20px; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555;">
+                                            Dear '.$guideFirstName.',
+                                            <br><br>
+                                            Welcome to Jourlaney, Your account has verified. Now you can chat and create order with tourist.
+                                            <br><br>
+                                            Yours faithfully,<br>
+                                            Jourlaney\'s team.
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
+                    <table align="center" role="presentation" cellspacing="0" cellpadding="0" border="0" width="600" style="margin: 0 auto;" class="email-container">
+                        <tr>
+                            <td style="padding: 20px; font-family: sans-serif; font-size: 12px; line-height: 15px; text-align: center; color: #888888;">
+                                <a href="http://jourlaney.com" style="color: #cccccc; text-decoration: underline; font-weight: bold;">Go to website</a>
+                                <br><br>
+                                Jourlaney<br><span class="unstyle-auto-detected-links">jourlaney.com</span>
+                            </td>
+                        </tr>
+                    </table>
+                </center>
+            </body>
+            </html>';
+            $mail->CharSet = 'UTF-8';
+
+            $mail->send();
+        } catch (Exception $e) {
+            echo 'Message not sent';
+        }
+
         echo "<script>window.alert('Verified.')</script>";
         return redirect('adminGetData');
+
+
     }
 }
