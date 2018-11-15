495f6cfc8e8bbbe1a46d44df8ba0a9d5f9549444
Erth <panot1997@hotmail.com>
2018-11-15 14:47:38 +0700

    Add birthday checking

--- app/Http/Controllers/GuideRegisterController.php
+++ app/Http/Controllers/GuideRegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 use App\Repositories\Eloquent\EloquentGuideRepository as guideRepo;
 
 class GuideRegisterController extends Controller
@@ -14,24 +15,29 @@ class GuideRegisterController extends Controller
         $password = $request->input('password');
         $hash = hash('sha512',$password);
         $userEmail = $request->input('email');
+        $today = Carbon::today('Asia/Bangkok')->isoFormat('YYYY-MM-DD');
 
         $checkUsername = DB::table('Users')->where(['username'=>$username])->get();
         $checkEmail = DB::table('Users')->where(['userEmail'=>$userEmail])->get();
         if(count($checkUsername) ==0){
             if(count($checkEmail) ==0){
-                $guideLicenseImage = $request->file('guidelicensepic');
-                $input['filename'] = time().'.'.$guideLicenseImage->getClientOriginalExtension();
-                $licensePath = public_path('/images/licensepic');
-                $guideLicenseImage->move($licensePath, $input['filename']);
-                $guideLicensePicName = $input['filename'];
+                if($request->input('birthdate') < $today){
+                    $guideLicenseImage = $request->file('guidelicensepic');
+                    $input['filename'] = time().'.'.$guideLicenseImage->getClientOriginalExtension();
+                    $licensePath = public_path('/images/licensepic');
+                    $guideLicenseImage->move($licensePath, $input['filename']);
+                    $guideLicensePicName = $input['filename'];
 
 
-                $queryUser = DB::insert("insert into Users(username,userPassword,userFirstName,userLastName,userEmail,userGender,userDOB,userIdcard) values(?,?,?,?,?,?,?,?)",[$username,$hash,$request->input('firstname'),$request->input('lastname'),$request->input('email'),$request->input('gender'),$request->input('birthdate'),$request->input('idcard')]);
-                $queryGuide = DB::insert("insert into Guide(username,guideLicenseNumber,guideLicensePic) value(?,?,?)",[$request->input('username'),$request->input('guidelicense'),$guideLicensePicName]);
-                
-                // $guideId = $getGuideId[0]->guideId;
-                // $queryBankAccount = DB::insert("insert into GuideBankAccount(guideId,bankAccountNumber,bankAccountBank) value(?,?,?)",[$guideId,$request->input('accountno'),$request->input('bankname')]);
-                return view('registercompleted');
+                    $queryUser = DB::insert("insert into Users(username,userPassword,userFirstName,userLastName,userEmail,userGender,userDOB,userIdcard) values(?,?,?,?,?,?,?,?)",[$username,$hash,$request->input('firstname'),$request->input('lastname'),$request->input('email'),$request->input('gender'),$request->input('birthdate'),$request->input('idcard')]);
+                    $queryGuide = DB::insert("insert into Guide(username,guideLicenseNumber,guideLicensePic) value(?,?,?)",[$request->input('username'),$request->input('guidelicense'),$guideLicensePicName]);
+                    
+                    // $guideId = $getGuideId[0]->guideId;
+                    // $queryBankAccount = DB::insert("insert into GuideBankAccount(guideId,bankAccountNumber,bankAccountBank) value(?,?,?)",[$guideId,$request->input('accountno'),$request->input('bankname')]);
+                    return view('registercompleted');
+                }
+                echo "<script>window.alert('Please use real birthday.')</script>";
+                return view('guideregister');
             }
             else{
                 echo "<script>window.alert('Dublicate email! Please change.')</script>";
@@ -43,6 +49,4 @@ class GuideRegisterController extends Controller
             return view('guideregister');
         }
     }
-
-
 }
--- app/Http/Controllers/TouristRegisterController.php
+++ app/Http/Controllers/TouristRegisterController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 use App\Models\TouristRegister;
 
 class TouristRegisterController extends Controller
@@ -15,29 +16,35 @@ class TouristRegisterController extends Controller
         $password = $request->input('password');
         $hash = hash('sha512',$password);
         $userEmail = $request->input('email');
+        $today = Carbon::today('Asia/Bangkok')->isoFormat('YYYY-MM-DD');
         
         $checkUsername = DB::table('Users')->where(['username'=>$username])->get();
         $checkEmail = DB::table('Users')->where(['userEmail'=>$userEmail])->get();
         if(count($checkUsername) ==0){
             if(count($checkEmail) ==0){
-                $touristIdCardImage = $request->file('idcardpic');
-                $input['filename'] = time().'.'.$touristIdCardImage->getClientOriginalExtension();
-                $picturePath = public_path('/images/idcardpic');
-                $touristIdCardImage->move($picturePath, $input['filename']);
-                $touristIdCardPicName = $input['filename'];
+                if($request->input('birthdate') < $today){
+                    $touristIdCardImage = $request->file('idcardpic');
+                    $input['filename'] = time().'.'.$touristIdCardImage->getClientOriginalExtension();
+                    $picturePath = public_path('/images/idcardpic');
+                    $touristIdCardImage->move($picturePath, $input['filename']);
+                    $touristIdCardPicName = $input['filename'];
 
-                $queryUser = DB::insert("insert into Users(username,userPassword,userFirstName,userLastName,userEmail,userGender,userDOB,userIdcard) values(?,?,?,?,?,?,?,?)",[$username,$hash,$request->input('firstname'),$request->input('lastname'),$request->input('email'),$request->input('gender'),$request->input('birthdate'),$request->input('idcard')]);
-                $queryTourist = DB::insert("insert into Tourist(username,touristIdCardPic) value(?,?)",[$request->input('username'),$touristIdCardPicName]);
-                return view('registercompleted');
+                    $queryUser = DB::insert("insert into Users(username,userPassword,userFirstName,userLastName,userEmail,userGender,userDOB,userIdcard) values(?,?,?,?,?,?,?,?)",[$username,$hash,$request->input('firstname'),$request->input('lastname'),$request->input('email'),$request->input('gender'),$request->input('birthdate'),$request->input('idcard')]);
+                    $queryTourist = DB::insert("insert into Tourist(username,touristIdCardPic) value(?,?)",[$request->input('username'),$touristIdCardPicName]);
+                    return view('registercompleted');
+                }
+                echo "<script>window.alert('Please use real birthday.')</script>";
+                return view('guideregister');
             }
             else{
                 echo "<script>window.alert('Dublicate email! Please change.')</script>";
-                return view('touristregister');
+                return view('guideregister');
             }
         }
         else{
             echo "<script>window.alert('Dublicate username! Please change.')</script>";
-            return view('touristregister');
+            return view('guideregister');
         }
     }
 }
+
